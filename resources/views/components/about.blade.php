c3dadd3cfcad8e3b15738683b3970ed0cb7de3fc
Mohamad Elhadidy <elhadidy.384@gmail.com>
2023-02-23 14:19:09 +0200

    finished heto & about sections

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -4,46 +4,23 @@
                 <div class="container custom-container">
                     <div class="about-inner">
                         <div class="row align-items-center justify-content-center">
-                            <div class="col-46 order-0 order-lg-2">
-                                <div class="about-img text-end">
+                            <div class="col-54 order-0 order-lg-2">
+                                <div class="about-content text-end">
+                                    <div class="section-title mb-25">
+                                        <span class="sub-title">نبذه عنا</span>
+                                        <h2 class="title">رواد صناعة المتاجر في العالم العربي</h2>
+                                    </div>
+                                    <p>
+                                        نعمل في مجال تقنية المعلومات في المملكة العربية السعودية منذ 8 سنوات خدمنا فيها اكثر من 5000 عميل في مجال تطوير وتصميم مواقع الانترنت وخاصة في مجال تصميم وتطوير المتاجر الالكترونية و تطبيقات الجوال المخصصة من خلال خدماتنا المميزة والفريدة ومشاريعنا التي تقدم كل الدعم اللوجيستي للمتاجر الالكترونية في المملكة والمنقطة العربية، نحرص وبشدة علي توفير جميع الحلول والأفكار الفريدة المتقدمة والتي تساعدك بشكل كبير في نجاح أعمالك ومتجرك الالكتروني بشكل قوي عبر توفير خدمات التدريب والإعلان وإدارة المحتوي والتسويق والتصوير وحلول إدارة علاقات العملاء ومراكز الاتصال وخدمة العملاء لمتجرك
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-46 ">
+                                <div class="about-img">
                                     <img src="/assets/img/images/about_img.jpg" alt="">
                                 </div>
                             </div>
-                            <div class="col-54">
-                                <div class="about-content">
-                                    <div class="section-title mb-25">
-                                        <span class="sub-title">About Company</span>
-                                        <h2 class="title">A Full Service Design and Branding Agency</h2>
-                                    </div>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
-                                    <ul class="list-wrap">
-                                        <li>
-                                            <div class="icon">
-                                                <img src="/assets/img/icon/about_icon01.png" alt="">
-                                            </div>
-                                            <div class="content">
-                                                <h4 class="title">Concept Creation</h4>
-                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="icon">
-                                                <img src="/assets/img/icon/about_icon02.png" alt="">
-                                            </div>
-                                            <div class="content">
-                                                <h4 class="title">Sketch Drawing</h4>
-                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                    <div class="about-content-bottom">
-                                        <span>Think Creative Agency Are a Full Service Design</span>
-                                        <div class="read-more-btn">
-                                            <a href="about-us.html" class="btn">Read More <span></span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        
                         </div>
                     </div>
                 </div>
--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -5,60 +5,18 @@
                         <div class="container">
                             <div class="row align-items-center">
                                 <div class="col-lg-6 order-0 order-lg-2">
-                                    <div class="slider-img text-end" data-animation="fadeInRight" data-delay=".8s">
-                                        <img src="/assets/img/slider/slider_img01.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="slider-content">
-                                        <h2 class="title" data-animation="fadeInUp" data-delay=".2s">Digital Agency Business Make Big Deal.</h2>
-                                        <p data-animation="fadeInUp" data-delay=".4s">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+                                    <div class="slider-content text-end">
+                                        <h2 class="title" data-animation="fadeInUp" data-delay=".2s"> أطلق نجاحك </h2>
+                                        <p data-animation="fadeInUp" data-delay=".4s">رواد صناعة المتاجر الإلكترونية ومواقع الإنترنت والتطبيقات لأكثر من 10 سنوات. </p>
                                         <div class="slider-btn">
-                                            <a href="contact.html" class="btn" data-animation="fadeInLeft" data-delay=".6s">Get Started <span></span></a>
-                                            <a href="https://www.youtube.com/watch?v=bixR-KIJKYM" class="popup-video" data-animation="fadeInRight" data-delay=".6s">See Live Demo <i class="fas fa-play pulse"></i></a>
+                                            {{-- <a href="contact.html" class="btn" data-animation="fadeInLeft" data-delay=".6s">Get Started <span></span></a> --}}
+                                            <a href="https://www.youtube.com/watch?v=bixR-KIJKYM" class="popup-video" data-animation="fadeInRight" data-delay=".6s"><i class="fas fa-play pulse"></i></a>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single-slider">
-                        <div class="container">
-                            <div class="row align-items-center">
-                                <div class="col-lg-6 order-0 order-lg-2">
-                                    <div class="slider-img text-end" data-animation="fadeInRight" data-delay=".8s">
+                                <div class="col-lg-6 ">
+                                    <div class="slider-img " data-animation="fadeInRight" data-delay=".8s">
                                         <img src="/assets/img/slider/slider_img01.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="slider-content">
-                                        <h2 class="title" data-animation="fadeInUp" data-delay=".2s">Digital Agency Business Make Big Deal.</h2>
-                                        <p data-animation="fadeInUp" data-delay=".4s">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
-                                        <div class="slider-btn">
-                                            <a href="contact.html" class="btn" data-animation="fadeInLeft" data-delay=".6s">Get Started <span></span></a>
-                                            <a href="https://www.youtube.com/watch?v=bixR-KIJKYM" class="popup-video" data-animation="fadeInRight" data-delay=".6s">See Live Demo <i class="fas fa-play pulse"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single-slider">
-                        <div class="container">
-                            <div class="row align-items-center">
-                                <div class="col-lg-6 order-0 order-lg-2">
-                                    <div class="slider-img text-end" data-animation="fadeInRight" data-delay=".8s">
-                                        <img src="/assets/img/slider/slider_img01.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="slider-content">
-                                        <h2 class="title" data-animation="fadeInUp" data-delay=".2s">Digital Agency Business Make Big Deal.</h2>
-                                        <p data-animation="fadeInUp" data-delay=".4s">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
-                                        <div class="slider-btn">
-                                            <a href="contact.html" class="btn" data-animation="fadeInLeft" data-delay=".6s">Get Started <span></span></a>
-                                            <a href="https://www.youtube.com/watch?v=bixR-KIJKYM" class="popup-video" data-animation="fadeInRight" data-delay=".6s">See Live Demo <i class="fas fa-play pulse"></i></a>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
